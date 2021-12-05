e66035844dfee0afb3cb7c513618b72cca12a352
Le Zhang <lezhang@unc.edu>
2021-12-04 21:12:43 -0500

    multi-layer

---
+++ main.h
@@ -0,0 +1,17 @@
+#include <layers/predict.h>
+#include <msp430.h>
+
+#include "DSPLib.h"
+#include "init.h"
+#include "neural_network_parameters.h"
+#include "math/matrix_ops.h"
+#include "math/fixed_point_ops.h"
+#include "math/matrix.h"
+#include "utils/utils.h"
+#include "layers/layers.h"
+#include "layers/trainable.h"
+
+#ifndef MAIN_GUARD
+#define MAIN_GUARD
+
+#endif
